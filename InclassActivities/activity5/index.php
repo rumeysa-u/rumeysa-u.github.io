3f0e11b4ad0a63b36cdcbfa2978567a72766ef2d
Rumeysa Uçar <ucar.rumeys@gmail.com>
2024-03-22 16:09:18 +0300

    activity5fix

--- InclassActivities/activity5/index.php
+++ InclassActivities/activity5/index.php
@@ -6,23 +6,26 @@
     <title>Activity 5 - Preview</title>
 </head>
 <body>
-    <?php
-    $fields = array(
-        'Name' => $_POST['name'] ?? 'Not provided',
-        'Username' => $_POST['username'] ?? 'Not provided',
-        'Password' => $_POST['password'] ?? 'Not provided',
-        'Address' => $_POST['address'] ?? 'Not provided',
-        'Country' => $_POST['country'] ?? 'Not provided',
-        'ZIP Code' => $_POST['zip'] ?? 'Not provided',
-        'Email' => $_POST['email'] ?? 'Not provided',
-        'Sex' => $_POST['sex'] ?? 'Not provided',
-        'Languages' => isset($_POST['languages']) ? implode(', ', $_POST['languages']) : 'Not provided',
-        'About' => $_POST['about'] ?? 'Not provided'
-    );
+	<?php
+	$fields = array(
+		'Name' => $_POST['name'] ?? '',
+		'Username' => $_POST['username'] ?? '',
+		'Password' => $_POST['password'] ?? '',
+		'Address' => $_POST['address'] ?? '',
+		'Country' => $_POST['country'] ?? '',
+		'ZIP Code' => $_POST['zip'] ?? '',
+		'Email' => $_POST['email'] ?? '',
+		'Sex' => $_POST['sex'] ?? '',
+		'Languages' => isset($_POST['languages']) ? implode(', ', $_POST['languages']) : '',
+		'About' => $_POST['about'] ?? ''
+	);
 
-    foreach ($fields as $label => $value) {
-        echo "<p><strong>$label:</strong> $value</p>";
-    }
-    ?>
+	foreach ($fields as $label => $value) {
+		if (empty($value)) {
+			$value = 'Not provided';
+		}
+		echo "<p><strong>$label:</strong> $value</p>";
+	}
+	?>
 </body>
 </html>
